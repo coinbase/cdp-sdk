036cf93d3bd00bdab4786473828823d773ddd857
Saliou Diallo <sddioulde@hotmail.com>
2025-05-08 16:14:09 -0400

    Update evm client sign typed data example

--- examples/python/evm/sign_typed_data.py
+++ examples/python/evm/sign_typed_data.py
@@ -9,8 +9,9 @@ from cdp.openapi_client.models.eip712_message import EIP712Message
 
 async def main():
     async with CdpClient() as cdp:
+        account = await cdp.evm.get_or_create_account(name="MyAccount")
         signature = await cdp.evm.sign_typed_data(
-            address="0x1234567890123456789012345678901234567890",
+            address=account.address,
             message=EIP712Message(
                 domain=EIP712Domain(
                     name="EIP712Domain",
