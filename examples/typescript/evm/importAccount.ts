5dde86297128c13cffae173cc98c9bb64ed915b4
Marcin Lenczewski <marcin.lenczewski@coinbase.com>
2025-05-15 13:11:14 -0400

    run prettier on importAccount example

--- examples/typescript/evm/importAccount.ts
+++ examples/typescript/evm/importAccount.ts
@@ -5,7 +5,7 @@ import "dotenv/config";
 
 const cdp = new CdpClient();
 const account = await cdp.evm.importAccount({
-    privateKey: "0x123456",
-    name: "MyAccount"
+  privateKey: "0x123456",
+  name: "MyAccount",
 });
 console.log("Imported account: ", account.address);
