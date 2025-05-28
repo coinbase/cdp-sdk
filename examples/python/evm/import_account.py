0ab62d5a551f923cf1690e8eca1e5a2115306930
Saliou Diallo <sddioulde@hotmail.com>
2025-05-28 11:11:44 -0400

    Update example

--- examples/python/evm/import_account.py
+++ examples/python/evm/import_account.py
@@ -12,11 +12,14 @@ load_dotenv()
 async def main():
     async with CdpClient() as cdp:
         account = Account.create()
-        account = await cdp.evm.import_account(
+        print("Account address: ", account.address)
+        imported_account = await cdp.evm.import_account(
             private_key=account.key.hex(),
             name="MyImportedAccount",
         )
-        print("Imported account: ", account.address)
+        print("Imported account: ", imported_account.address)
+        retrieved_account = await cdp.evm.get_account(address=imported_account.address)
+        print("Retrieved account: ", retrieved_account.address)
 
 
 asyncio.run(main())
