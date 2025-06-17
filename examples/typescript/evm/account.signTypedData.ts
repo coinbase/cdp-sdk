e817272e9b83aff9ec6e92a4d456773d91177e93
Ryan Gilbert <ryangilbert7926@gmail.com>
2025-06-17 12:40:44 -0400

    chore: add example build step to ci

--- examples/typescript/evm/account.signTypedData.ts
+++ examples/typescript/evm/account.signTypedData.ts
@@ -12,7 +12,7 @@ console.log("Created account:", account.address);
 const signature = await account.signTypedData({
   domain: {
     name: "Permit2",
-    chainId: 1,
+    chainId: 1n,
     verifyingContract: "0x000000000022D473030F116dDEE9F6B43aC78BA3",
   },
   types: {
@@ -36,11 +36,11 @@ const signature = await account.signTypedData({
   message: {
     permitted: {
       token: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
-      amount: "1000000",
+      amount: 1000000n,
     },
     spender: "0xFfFfFfFFfFFfFFfFFfFFFFFffFFFffffFfFFFfFf",
-    nonce: "0",
-    deadline: "1717123200",
+    nonce: 0n,
+    deadline: 1717123200n,
   },
 });
 
