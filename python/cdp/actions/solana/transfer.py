ee7601ce9700272162684435721f42bc9e0af3b5
Saliou Diallo <sddioulde@hotmail.com>
2025-07-22 10:51:16 -0400

    chore: Use CDP SendSolanaTransaction API for transfer action (#309)

--- python/cdp/actions/solana/transfer.py
+++ python/cdp/actions/solana/transfer.py
@@ -1,10 +1,11 @@
 import base64
 
 from solana.rpc.api import Client as SolanaClient
-from solana.rpc.types import TxOpts
+from solders.hash import Hash
 from solders.message import Message
 from solders.pubkey import Pubkey as PublicKey
 from solders.system_program import TransferParams, transfer as solders_transfer
+from solders.transaction import Transaction
 from spl.token.constants import TOKEN_PROGRAM_ID
 from spl.token.instructions import (
     TransferCheckedParams,
@@ -13,9 +14,13 @@ from spl.token.instructions import (
 )
 
 from cdp.actions.solana.types import TransferOptions
-from cdp.actions.solana.utils import get_or_create_connection, get_usdc_mint_address
+from cdp.actions.solana.utils import (
+    get_connected_network,
+    get_or_create_connection,
+    get_usdc_mint_address,
+)
 from cdp.api_clients import ApiClients
-from cdp.openapi_client.models.sign_solana_transaction_request import SignSolanaTransactionRequest
+from cdp.openapi_client.models.send_solana_transaction_request import SendSolanaTransactionRequest
 
 
 async def transfer(
@@ -33,18 +38,15 @@ async def transfer(
 
     """
     connection = get_or_create_connection(options.network)
+    connected_network = await get_connected_network(connection)
 
     if options.token == "sol":
-        tx_bytes = await get_native_transfer(
-            connection, options.from_account, options.to_account, options.amount
-        )
+        tx = await get_native_transfer(options.from_account, options.to_account, options.amount)
     else:
         mint_address = (
-            get_usdc_mint_address(options.network)
-            if options.token == "usdc"
-            else options.mint_address
+            get_usdc_mint_address(connected_network) if options.token == "usdc" else options.token
         )
-        tx_bytes = get_spl_transfer(
+        tx = get_spl_transfer(
             connection,
             options.from_account,
             options.to_account,
@@ -52,56 +54,36 @@ async def transfer(
             options.amount,
         )
 
-    serialized_tx = base64.b64encode(tx_bytes).decode("utf-8")
-    response = await api_clients.solana_accounts.sign_solana_transaction(
-        address=options.from_account,
-        sign_solana_transaction_request=SignSolanaTransactionRequest(
+    serialized_tx = base64.b64encode(bytes(tx)).decode("utf-8")
+    response = await api_clients.solana_accounts.send_solana_transaction(
+        send_solana_transaction_request=SendSolanaTransactionRequest(
+            network="solana" if connected_network == "mainnet" else "solana-devnet",
             transaction=serialized_tx,
         ),
     )
-    signed_tx = response.signed_transaction
-    decoded_signed_tx = base64.b64decode(signed_tx)
 
-    tx_resp = connection.send_raw_transaction(
-        decoded_signed_tx, opts=TxOpts(skip_preflight=False, preflight_commitment="processed")
-    )
-
-    return tx_resp.value
+    return response.transaction_signature
 
 
 async def get_native_transfer(
-    connection: SolanaClient,
     from_account: str,
     to_account: str,
     amount: int,
-) -> bytes:
+) -> Transaction:
     """Get the trasnaction bytes for a native transfer."""
     source_pubkey = PublicKey.from_string(from_account)
     dest_pubkey = PublicKey.from_string(to_account)
-
-    blockhash_resp = connection.get_latest_blockhash()
-    blockhash = blockhash_resp.value.blockhash
 
     transfer_params = TransferParams(
         from_pubkey=source_pubkey, to_pubkey=dest_pubkey, lamports=amount
     )
     transfer_instr = solders_transfer(transfer_params)
 
-    message = Message.new_with_blockhash(
-        [transfer_instr],
-        source_pubkey,
-        blockhash,
-    )
+    placeholder_blockhash = Hash.from_string("SysvarRecentB1ockHashes11111111111111111111")
+    message = Message.new_with_blockhash([transfer_instr], source_pubkey, placeholder_blockhash)
+    tx = Transaction.new_unsigned(message)
 
-    # Create a transaction envelope with signature space
-    sig_count = bytes([1])  # 1 byte for signature count (1)
-    empty_sig = bytes([0] * 64)  # 64 bytes of zeros for the empty signature
-    message_bytes = bytes(message)  # Get the serialized message bytes
-
-    # Concatenate to form the transaction bytes
-    tx_bytes = sig_count + empty_sig + message_bytes
-
-    return tx_bytes
+    return tx
 
 
 def get_spl_transfer(
@@ -110,7 +92,7 @@ def get_spl_transfer(
     to_account: str,
     mint_address: str,
     amount: int,
-) -> bytes:
+) -> Transaction:
     """Get the transaction bytes for an SPL token transfer.
 
     Args:
@@ -184,22 +166,8 @@ def get_spl_transfer(
             ),
         )
     )
+    placeholder_blockhash = Hash.from_string("SysvarRecentB1ockHashes11111111111111111111")
+    message = Message.new_with_blockhash(instructions, from_pubkey, placeholder_blockhash)
+    tx = Transaction.new_unsigned(message)
 
-    blockhash_resp = connection.get_latest_blockhash()
-    blockhash = blockhash_resp.value.blockhash
-
-    message = Message.new_with_blockhash(
-        instructions,
-        from_pubkey,
-        blockhash,
-    )
-
-    # Create transaction bytes
-    sig_count = bytes([1])  # 1 byte for signature count (1)
-    empty_sig = bytes([0] * 64)  # 64 bytes of zeros for the empty signature
-    message_bytes = bytes(message)  # Get the serialized message bytes
-
-    # Concatenate to form the transaction bytes
-    tx_bytes = sig_count + empty_sig + message_bytes
-
-    return tx_bytes
+    return tx
---
+++ typescript/.changeset/giant-cycles-take.md
@@ -0,0 +1,5 @@
+---
+"@coinbase/cdp-sdk": patch
+---
+
+Updated transfer to use SendSolanaTransaction
