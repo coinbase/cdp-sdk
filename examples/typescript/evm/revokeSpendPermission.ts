3381435a9838363d03495071df828913c3453fa5
milan-cb <milan.saxena@coinbase.com>
2025-08-01 15:57:31 -0700

    feat(CDPSDK-1244): add list & revoke spend permission (#339)

---
+++ examples/typescript/evm/revokeSpendPermission.ts
@@ -0,0 +1,57 @@
+import { CdpClient, SpendPermission } from "@coinbase/cdp-sdk";
+import { parseEther } from "viem";
+import "dotenv/config";
+
+const cdp = new CdpClient();
+
+const owner = await cdp.evm.createAccount();
+
+const smartAccount = await cdp.evm.createSmartAccount({
+  owner,
+  __experimental_enableSpendPermission: true,
+});
+
+const spender = await cdp.evm.createAccount();
+
+const spendPermission: SpendPermission = {
+  account: smartAccount.address,
+  spender: spender.address,
+  token: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+  allowance: parseEther("0.00001"),
+  period: 86400,
+  start: 0,
+  end: 281474976710655,
+  salt: BigInt(0),
+  extraData: "0x",
+};
+
+const { userOpHash } = await cdp.evm.createSpendPermission({
+  spendPermission,
+  network: "base-sepolia",
+});
+
+const userOperationResult = await cdp.evm.waitForUserOperation({
+  smartAccountAddress: smartAccount.address,
+  userOpHash,
+});
+
+console.log("User Operation:", userOperationResult);
+
+const permissions = await cdp.evm.listSpendPermissions({
+  address: smartAccount.address,
+});
+
+const permissionHash = permissions.spendPermissions[0].permissionHash;
+
+const { userOpHash: revokeUserOpHash } = await cdp.evm.revokeSpendPermission({
+  address: smartAccount.address,
+  permissionHash: permissionHash as `0x${string}`,
+  network: "base-sepolia",
+});
+
+const revokeUserOperationResult = await cdp.evm.waitForUserOperation({
+  smartAccountAddress: smartAccount.address,
+  userOpHash: revokeUserOpHash,
+});
+
+console.log("Revoke User Operation:", revokeUserOperationResult);
