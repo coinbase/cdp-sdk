84b1f43db9306d69547c0dab66d92f9955764475
Andrew Reder <avidreder@gmail.com>
2025-08-06 09:51:46 -0400

    initial impl of validate access token in ts sdk

---
+++ typescript/src/client/end-user/endUser.types.ts
@@ -0,0 +1,10 @@
+export interface ValidateAccessTokenOptions {
+  /**
+   * The access token to validate.
+   */
+  accessToken: string;
+  /**
+   * The user ID to validate the access token for.
+   */
+  userId: string;
+}
--- typescript/src/openapi-client/index.ts
+++ typescript/src/openapi-client/index.ts
@@ -10,8 +10,10 @@ export * from "./generated/policy-engine/policy-engine.js";
 export * from "./generated/payments-alpha/payments-alpha.js";
 export * from "./generated/onramp/onramp.js";
 export * from "./generated/onchain-data/onchain-data.js";
+export * from "./generated/end-user-accounts-under-development/end-user-accounts-under-development.js";
 
 import { configure } from "./cdpApiClient.js";
+import * as endUserAccounts from "./generated/end-user-accounts-under-development/end-user-accounts-under-development.js";
 import * as evm from "./generated/evm-accounts/evm-accounts.js";
 import * as evmSmartAccounts from "./generated/evm-smart-accounts/evm-smart-accounts.js";
 import * as evmSwaps from "./generated/evm-swaps/evm-swaps.js";
@@ -32,6 +34,7 @@ export const CdpOpenApiClient = {
   ...faucets,
   ...policies,
   ...payments,
+  ...endUserAccounts,
   configure,
 };
 
