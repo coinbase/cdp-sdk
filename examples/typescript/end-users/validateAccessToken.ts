1100dc28fc29706167e5175e08a9c4cceac74a64
Andrew Reder <avidreder@gmail.com>
2025-08-08 07:14:46 -0400

    better example

--- examples/typescript/end-users/validateAccessToken.ts
+++ examples/typescript/end-users/validateAccessToken.ts
@@ -1,27 +1,22 @@
-// Usage: CDP_ACCESS_TOKEN=... CDP_USER_ID=... pnpm tsx end-users/validateAccessToken.ts
+// Usage: CDP_ACCESS_TOKEN=... pnpm tsx end-users/validateAccessToken.ts
 
 import { CdpClient } from "@coinbase/cdp-sdk";
 import "dotenv/config";
 
 const cdp = new CdpClient({
     basePath: "https://cloud-api-dev.cbhq.net/platform",
-    debugging: true,
 });
-
-const userId = process.env.CDP_USER_ID;
-if (!userId) {
-    throw new Error("CDP_USER_ID is not set");
-}
 
 const accessToken = process.env.CDP_ACCESS_TOKEN;
 if (!accessToken) {
     throw new Error("CDP_ACCESS_TOKEN is not set");
 }
 
-console.log("client: ", cdp);
-
-const response = await cdp.endUser.validateAccessToken({
-    accessToken,
-    userId,
-});
-console.log("Access token validated: ", response);
+try {
+    const endUser = await cdp.endUser.validateAccessToken({
+        accessToken,
+    });
+    console.log("Access token validated: ", endUser);
+} catch (error) {
+    console.error("Error validating access token: ", (error as { errorMessage: string }).errorMessage);
+}
